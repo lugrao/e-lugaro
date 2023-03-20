5f48a29d03e6691d41a9152df9dd48dcd4136797
e <elugrao@gmail.com>
2023-03-20 18:21:59 -0300

    import PersonalInfo, Tools and Projects; render basic page structure

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { PersonalInfo } from "components/PersonalInfo"
+import { Tools } from "components/Tools"
+import { Projects } from "components/Projects"
+
+export default function Home() {
+  return (
+    <>
+      <PersonalInfo />
+      <Tools />
+      <Projects />
+    </>
+  )
+}
