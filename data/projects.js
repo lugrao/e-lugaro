c546fcb80b076d479c9d62688836231725099aac
e <elugrao@gmail.com>
2023-03-21 12:33:46 -0300

    add projects data

---
+++ data/projects.js
@@ -0,0 +1,43 @@
+export const projects = [
+  {
+    title: "Movie Ratings",
+    url: "https://movie-ratings.vercel.app/",
+    description:
+      "Web application that allows you to search for movies and their ratings from IMDb, RottenTomatoes, Metacritic, Letterboxd, TMDb and FilmAffinity",
+    madeWith: ["Flask"],
+    deployedTo: "Vercel",
+    sourceCodeUrl: "https://github.com/lugrao/movie_ratings",
+  },
+  {
+    title: "Chat App",
+    url: "https://chat-app-lugrao.vercel.app/",
+    description: "Chat appliaction",
+    madeWith: ["Next.js", "Ably"],
+    deployedTo: "Vercel",
+    sourceCodeUrl: "https://github.com/lugrao/chat-app",
+  },
+  {
+    title: "NASA Exoplanet Query",
+    url: "https://nasa-exoplanet-query.vercel.app/",
+    description: "Web application for querying NASA's Exoplanet Archive",
+    madeWith: ["Next.js", "Chakra UI", "React Virtual"],
+    deployedTo: "Vercel",
+    sourceCodeUrl: "https://github.com/lugrao/nasa-exoplanet-query",
+  },
+  {
+    title: "Yunga",
+    url: "https://www.yunga.musica.ar/",
+    description: "Landing page for Yunga, a really cool band from La Plata",
+    madeWith: ["Next.js"],
+    deployedTo: "Vercel",
+    sourceCodeUrl: "https://github.com/lugrao/yunga-web",
+  },
+  {
+    title: "Cadex",
+    url: "https://cadex.vercel.app/",
+    description: "Web application for writing along with other people",
+    madeWith: ["Next.js", "Chakra UI", "Auth0", "MongoDB"],
+    deployedTo: "Vercel",
+    sourceCodeUrl: "https://github.com/lugrao/cadex",
+  },
+]
