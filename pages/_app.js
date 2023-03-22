fa40537deeb69bfc4043a7fe9bf7f3e4e1a5dfa6
e <elugrao@gmail.com>
2023-03-22 18:13:33 -0300

    add font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import "styles/global.css"
+import { Merriweather_Sans } from "next/font/google"
+
+const merriweather_sans = Merriweather_Sans({ subsets: ["latin"] })
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={merriweather_sans.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
