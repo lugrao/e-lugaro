2649a424b5de8731875cfe2e641e386b68dbd03c
e <elugrao@gmail.com>
2023-03-22 17:39:46 -0300

    accept props; forward props to enclosing div; remove unnecessary styles

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,14 +1,14 @@
 import { projects } from "data/projects"
 import { List } from "components/List"
 
-export function Projects() {
+export function Projects(props) {
   return (
-    <div>
+    <div {...props}>
       <h2>Projects</h2>
       {projects.map((project, index) => (
         <div key={index}>
           <h3>{project.title}</h3>
-          <p css={{ overflowWrap: "break-word" }}>{project.description}</p>
+          <p>{project.description}</p>
           <List items={project.additionalInfoList} />
         </div>
       ))}
