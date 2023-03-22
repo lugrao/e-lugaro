abde62d8df3679c088dc7e475be4ecf88bd25bfd
e <elugrao@gmail.com>
2023-03-22 17:40:12 -0300

    accept props; forward props to enclosing div

--- components/Tools.jsx
+++ components/Tools.jsx
@@ -1,9 +1,9 @@
 import { List } from "components/List"
 import { tools } from "data/tools"
 
-export function Tools() {
+export function Tools(props) {
   return (
-    <div>
+    <div {...props}>
       <h2>Tools</h2>
       <List items={tools} />
     </div>
