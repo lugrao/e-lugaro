71977b2ffe128a136c44f0f943e88e613b60fa81
e <elugrao@gmail.com>
2023-03-22 11:54:09 -0300

    adapt structure to fit List component

--- data/projects.js
+++ data/projects.js
@@ -4,40 +4,76 @@ export const projects = [
     url: "https://movie-ratings.vercel.app/",
     description:
       "Web application that allows you to search for movies and their ratings from IMDb, RottenTomatoes, Metacritic, Letterboxd, TMDb and FilmAffinity",
-    madeWith: ["Flask"],
-    deployedTo: "Vercel",
-    sourceCodeUrl: "https://github.com/lugrao/movie_ratings",
+    additionalInfoList: [
+      "Made with Flask",
+      "Deployed via Vercel",
+      <>
+        Source code:{" "}
+        <a href="https://github.com/lugrao/movie_ratings">
+          github.com/lugrao/movie_ratings
+        </a>
+      </>,
+    ],
   },
   {
     title: "Chat App",
     url: "https://chat-app-lugrao.vercel.app/",
-    description: "Chat appliaction",
-    madeWith: ["Next.js", "Ably"],
-    deployedTo: "Vercel",
-    sourceCodeUrl: "https://github.com/lugrao/chat-app",
+    description: "Chat application",
+    additionalInfoList: [
+      "Made with",
+      ["Next.js", "Ably"],
+      "Deployed via Vercel",
+      <>
+        Source code:{" "}
+        <a href="https://github.com/lugrao/chat-app">
+          github.com/lugrao/chat-app
+        </a>
+      </>,
+    ],
   },
   {
     title: "NASA Exoplanet Query",
     url: "https://nasa-exoplanet-query.vercel.app/",
     description: "Web application for querying NASA's Exoplanet Archive",
-    madeWith: ["Next.js", "Chakra UI", "React Virtual"],
-    deployedTo: "Vercel",
-    sourceCodeUrl: "https://github.com/lugrao/nasa-exoplanet-query",
+    additionalInfoList: [
+      "Made with",
+      ["Next.js", "Chakra UI", "React Virtual"],
+      "Deployed via Vercel",
+      <>
+        Source code:{" "}
+        <a href="https://github.com/lugrao/nasa-exoplanet-query">
+          github.com/lugrao/nasa-exoplanet-query
+        </a>
+      </>,
+    ],
   },
   {
     title: "Yunga",
     url: "https://www.yunga.musica.ar/",
     description: "Landing page for Yunga, a really cool band from La Plata",
-    madeWith: ["Next.js"],
-    deployedTo: "Vercel",
-    sourceCodeUrl: "https://github.com/lugrao/yunga-web",
+    additionalInfoList: [
+      "Made with Next.js",
+      "Deployed via Vercel",
+      <>
+        Source code:{" "}
+        <a href="https://github.com/lugrao/yunga-web">
+          github.com/lugrao/yunga-web
+        </a>
+      </>,
+    ],
   },
   {
     title: "Cadex",
     url: "https://cadex.vercel.app/",
     description: "Web application for writing along with other people",
-    madeWith: ["Next.js", "Chakra UI", "Auth0", "MongoDB"],
-    deployedTo: "Vercel",
-    sourceCodeUrl: "https://github.com/lugrao/cadex",
+    additionalInfoList: [
+      "Made with",
+      ["Next.js", "Chakra UI", "Auth0", "MongoDB"],
+      "Deployed via Vercel",
+      <>
+        Source code:{" "}
+        <a href="https://github.com/lugrao/cadex">github.com/lugrao/cadex</a>
+      </>,
+    ],
   },
 ]
