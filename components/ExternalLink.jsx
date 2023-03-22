28b6285a8f34883cf364b28437aab3ee759798c1
e <elugrao@gmail.com>
2023-03-22 15:23:05 -0300

    remove link underline

--- components/ExternalLink.jsx
+++ components/ExternalLink.jsx
@@ -1,5 +1,11 @@
 export const ExternalLink = ({ children, href, ...props }) => (
-  <a href={href} target="_blank" {...props}>
+  <a
+    href={href}
+    target="_blank"
+    {...props}
+    css={{ textDecoration: "none" }}
+    {...props}
+  >
     {children}
   </a>
 )
