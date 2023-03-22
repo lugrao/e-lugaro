7603d1a00877ef3f90a37c09d8834b7def003cf2
e <elugrao@gmail.com>
2023-03-22 14:51:35 -0300

    add icons

--- data/tools.js
+++ data/tools.js
@@ -1,10 +1,45 @@
+import {
+  JavaScript,
+  React,
+  Nextjs,
+  Python,
+  Flask,
+  HTML,
+  CSS,
+  MongoDB,
+} from "components/icons/lib"
+
 export const tools = [
-  "JavaScript",
-  ["React", ["Next.js"]],
-  "Python",
-  ["Flask", "Django"],
-  "HTML",
-  "CSS",
-  "MongoDB",
+  <>
+    JavaScript <JavaScript />
+  </>,
+  [
+    <>
+      React <React />
+    </>,
+    [
+      <>
+        Next.js <Nextjs />
+      </>,
+    ],
+  ],
+  <>
+    Python <Python />
+  </>,
+  [
+    <>
+      Flask <Flask />
+    </>,
+    "Django",
+  ],
+  <>
+    HTML <HTML />
+  </>,
+  <>
+    CSS <CSS />
+  </>,
+  <>
+    MongoDB <MongoDB />
+  </>,
   "SQL",
 ]
