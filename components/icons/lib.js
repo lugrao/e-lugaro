b5012d254b52cce98d9acb36e2d8073f2994b3fe
e <elugrao@gmail.com>
2023-03-27 11:11:56 -0300

    add alt to images

--- components/icons/lib.js
+++ components/icons/lib.js
@@ -21,24 +21,34 @@ import vercel from "public/icons/Vercel.png"
 
 const Img = (props) => <Image width={15} height={15} {...props} />
 
-const Ably = (props) => <Img {...props} src={ably} />
-const Auth0 = (props) => <Img {...props} src={auth0} />
-const ChakraUI = (props) => <Img {...props} src={chakraui} />
-const Checkmark = (props) => <Img {...props} src={checkmark} />
-const CSS = (props) => <Img {...props} src={css} />
-const Email = (props) => <Img {...props} src={email} />
-const External = (props) => <Img {...props} src={external} />
-const Flask = (props) => <Img {...props} src={flask} />
-const GitHub = (props) => <Img {...props} src={github} />
-const HTML = (props) => <Img {...props} src={html} />
-const JavaScript = (props) => <Img {...props} src={javascript} />
-const LinkedIn = (props) => <Img {...props} src={linkedin} />
-const MongoDB = (props) => <Img {...props} src={mongodb} />
-const Nextjs = (props) => <Img {...props} src={nextjs} />
-const Python = (props) => <Img {...props} src={python} />
-const React = (props) => <Img {...props} src={react} />
-const SQL = (props) => <Img {...props} src={sql} />
-const Vercel = (props) => <Img {...props} src={vercel} />
+const Ably = (props) => <Img {...props} src={ably} alt="Ably icon" />
+const Auth0 = (props) => <Img {...props} src={auth0} alt="Auth0 icon" />
+const ChakraUI = (props) => (
+  <Img {...props} src={chakraui} alt="ChakraUI icon" />
+)
+const Checkmark = (props) => (
+  <Img {...props} src={checkmark} alt="Checkmark icon" />
+)
+const CSS = (props) => <Img {...props} src={css} alt="CSS icon" />
+const Email = (props) => <Img {...props} src={email} alt="Email icon" />
+const External = (props) => (
+  <Img {...props} src={external} alt="External Link icon" />
+)
+const Flask = (props) => <Img {...props} src={flask} alt="Flask icon" />
+const GitHub = (props) => <Img {...props} src={github} alt="GitHub icon" />
+const HTML = (props) => <Img {...props} src={html} alt="HTML icon" />
+const JavaScript = (props) => (
+  <Img {...props} src={javascript} alt="JavaScript icon" />
+)
+const LinkedIn = (props) => (
+  <Img {...props} src={linkedin} alt="LinkedIn icon" />
+)
+const MongoDB = (props) => <Img {...props} src={mongodb} alt="MongoDB icon" />
+const Nextjs = (props) => <Img {...props} src={nextjs} alt="Next.js icon" />
+const Python = (props) => <Img {...props} src={python} alt="Python icon" />
+const React = (props) => <Img {...props} src={react} alt="React icon" />
+const SQL = (props) => <Img {...props} src={sql} alt="SQL icon" />
+const Vercel = (props) => <Img {...props} src={vercel} alt="Vercel icon" />
 
 export {
   Ably,
