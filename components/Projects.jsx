bee7481272811d96308be169f0fc1f779fd09675
e <elugrao@gmail.com>
2023-03-27 11:00:04 -0300

    move contact information to separate component

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,15 +1,14 @@
-import { projects } from "data/projects"
-import { List } from "components/List"
+import { projectList } from "components/projectList"
 
 export function Projects(props) {
   return (
     <div {...props}>
       <h2>Projects</h2>
-      {projects.map((project, index) => (
+      {projectList.map((project, index) => (
         <div key={index}>
           <h3>{project.title}</h3>
           <p>{project.description}</p>
-          <List items={project.additionalInfoList} />
+          {project.additionalInfoList}
         </div>
       ))}
     </div>
