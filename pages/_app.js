53da9de109f1ded40ca724158d7abef6f86b77b2
lugrao <elugrao@gmail.com>
2023-05-05 10:47:17 -0300

    add theme provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import "styles/global.css"
 import { Merriweather_Sans } from "next/font/google"
+import { ThemeProvider } from "utils/ThemeProvider"
 
 const merriweather_sans = Merriweather_Sans({ subsets: ["latin"] })
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <main className={merriweather_sans.className}>
-      <Component {...pageProps} />
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </main>
   )
 }
