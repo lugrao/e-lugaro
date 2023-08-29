0d7b82edb62d84f87cf415f1755930c955eb3c9a
lugrao <elugrao@gmail.com>
2023-08-29 18:37:06 -0300

    Add Git and React Testing Library icons. Create Icon component and use it with all icons

--- components/icons/lib.js
+++ components/icons/lib.js
@@ -1,132 +1,94 @@
 /** @jsxImportSource @emotion/react */
 
-import Image from "next/image"
+import Image from "next/image";
 
-import { base64Icon } from "public/icons/base64icon"
-import ably from "public/icons/Ably.webp"
-import auth0 from "public/icons/Auth0.svg"
-import chakraui from "public/icons/Chakra-UI.webp"
-import checkmark from "public/icons/Checkmark.svg"
-import css from "public/icons/CSS.svg"
-import django from "public/icons/Django.png"
-import email from "public/icons/Email.svg"
-import external from "public/icons/External.svg"
-import flask from "public/icons/Flask.svg"
-import github from "public/icons/GitHub.svg"
-import html from "public/icons/HTML.svg"
-import javascript from "public/icons/JavaScript.svg"
-import linkedin from "public/icons/LinkedIn.svg"
-import mongodb from "public/icons/MongoDB.svg"
-import nextjs from "public/icons/Next.js.svg"
-import python from "public/icons/Python.svg"
-import react from "public/icons/React.svg"
-import sql from "public/icons/SQL.png"
-import tanstack from "public/icons/TanStack.png"
-import vercel from "public/icons/Vercel.png"
+import { base64Icon } from "public/icons/base64icon";
+import ably from "public/icons/Ably.webp";
+import auth0 from "public/icons/Auth0.svg";
+import chakraui from "public/icons/Chakra-UI.webp";
+import checkmark from "public/icons/Checkmark.svg";
+import css from "public/icons/CSS.svg";
+import django from "public/icons/Django.svg";
+import email from "public/icons/Email.svg";
+import external from "public/icons/External.svg";
+import flask from "public/icons/Flask.svg";
+import git from "public/icons/Git.svg";
+import github from "public/icons/GitHub.svg";
+import html from "public/icons/HTML.svg";
+import javascript from "public/icons/JavaScript.svg";
+import linkedin from "public/icons/LinkedIn.svg";
+import mongodb from "public/icons/MongoDB.svg";
+import nextjs from "public/icons/Next.js.svg";
+import python from "public/icons/Python.svg";
+import react from "public/icons/React.svg";
+import reacttestinglibrary from "public/icons/React Testing Library.svg";
+import sql from "public/icons/SQL.svg";
+import tanstack from "public/icons/TanStack.png";
+import vercel from "public/icons/Vercel.png";
+
+import { useTheme } from "utils/useTheme";
 
 const imageProps = {
   blurDataURL: base64Icon,
   width: 15,
   height: 15,
-}
+};
 
-const Ably = (props) => (
-  <Image {...imageProps} src={ably} alt="Ably icon" {...props} />
-)
-const Auth0 = (props) => (
-  <Image {...imageProps} src={auth0} alt="Auth0 icon" {...props} />
-)
+const Icon = ({ src, alt, width = 15, height = 15, ...props }) => {
+  const { colorMode } = useTheme();
+  return (
+    <Image
+      blurDataURL={base64Icon}
+      width={width}
+      height={height}
+      src={src}
+      alt={alt}
+      css={{ filter: colorMode === "dark" && "invert(100%)" }}
+      {...props}
+    />
+  );
+};
+
+const Ably = (props) => <Icon src={ably} alt="Ably icon" {...props} />;
+const Auth0 = (props) => <Icon src={auth0} alt="Auth0 icon" {...props} />;
 const ChakraUI = (props) => (
-  <Image
-    {...imageProps}
+  <Icon
     src={chakraui}
     alt="ChakraUI icon"
     css={{ borderRadius: "50%" }}
     {...props}
   />
-)
+);
 const Checkmark = (props) => (
-  <Image {...imageProps} src={checkmark} alt="Checkmark icon" {...props} />
-)
-const CSS = (props) => (
-  <Image {...imageProps} src={css} alt="CSS icon" {...props} />
-)
-const Django = (props) => (
-  <Image {...imageProps} src={django} alt="Django icon" {...props} />
-)
-const Email = (props) => (
-  <Image
-    {...imageProps}
-    src={email}
-    alt="Email icon"
-    {...props}
-  />
-)
+  <Icon src={checkmark} alt="Checkmark icon" {...props} />
+);
+const CSS = (props) => <Icon src={css} alt="CSS icon" {...props} />;
+const Django = (props) => <Icon src={django} alt="Django icon" {...props} />;
+const Email = (props) => <Icon src={email} alt="Email icon" {...props} />;
 const External = (props) => (
-  <Image {...imageProps} src={external} alt="External Link icon" {...props} />
-)
-const Flask = (props) => (
-  <Image
-    {...imageProps}
-    src={flask}
-    alt="Flask icon"
-    css={{ backgroundColor: "white", borderRadius: "30%" }}
-    {...props}
-  />
-)
-const GitHub = (props) => (
-  <Image
-    {...imageProps}
-    src={github}
-    alt="GitHub icon"
-    {...props}
-  />
-)
-const HTML = (props) => (
-  <Image {...imageProps} src={html} alt="HTML icon" {...props} />
-)
+  <Icon src={external} alt="External Link icon" {...props} />
+);
+const Flask = (props) => <Icon src={flask} alt="Flask icon" {...props} />;
+const Git = (props) => <Icon src={git} alt="Git icon" {...props} />;
+const GitHub = (props) => <Icon src={github} alt="GitHub icon" {...props} />;
+const HTML = (props) => <Icon src={html} alt="HTML icon" {...props} />;
 const JavaScript = (props) => (
-  <Image {...imageProps} src={javascript} alt="JavaScript icon" {...props} />
-)
+  <Icon src={javascript} alt="JavaScript icon" {...props} />
+);
 const LinkedIn = (props) => (
-  <Image
-    {...imageProps}
-    src={linkedin}
-    alt="LinkedIn icon"
-    {...props}
-  />
-)
-const MongoDB = (props) => (
-  <Image {...imageProps} src={mongodb} alt="MongoDB icon" {...props} />
-)
-const Nextjs = (props) => (
-  <Image
-    {...imageProps}
-    src={nextjs}
-    alt="Next.js icon"
-    css={{ backgroundColor: "white", borderRadius: "50%" }}
-    {...props}
-  />
-)
-const Python = (props) => (
-  <Image {...imageProps} src={python} alt="Python icon" {...props} />
-)
-const React = (props) => (
-  <Image {...imageProps} src={react} alt="React icon" {...props} />
-)
-const SQL = (props) => (
-  <Image
-    {...imageProps}
-    width={12}
-    height={15}
-    src={sql}
-    alt="SQL icon"
-    {...props}
-  />
-)
+  <Icon src={linkedin} alt="LinkedIn icon" {...props} />
+);
+const MongoDB = (props) => <Icon src={mongodb} alt="MongoDB icon" {...props} />;
+const Nextjs = (props) => <Icon src={nextjs} alt="Next.js icon" {...props} />;
+const Python = (props) => <Icon src={python} alt="Python icon" {...props} />;
+const React = (props) => <Icon src={react} alt="React icon" {...props} />;
+const ReactTestingLibrary = (props) => (
+  <Icon src={reacttestinglibrary} alt="React Testing Library icon" {...props} />
+);
+const SQL = (props) => <Icon src={sql} alt="SQL icon" {...props} />;
 const TanStack = (props) => (
   <Image {...imageProps} src={tanstack} alt="TanStack icon" {...props} />
-)
+);
 const Vercel = (props) => (
   <Image
     {...imageProps}
@@ -135,7 +97,7 @@ const Vercel = (props) => (
     css={{ borderRadius: "50%" }}
     {...props}
   />
-)
+);
 
 export {
   Ably,
@@ -147,6 +109,7 @@ export {
   Email,
   External,
   Flask,
+  Git,
   GitHub,
   HTML,
   JavaScript,
@@ -155,7 +118,8 @@ export {
   Nextjs,
   Python,
   React,
+  ReactTestingLibrary,
   SQL,
   TanStack,
   Vercel,
-}
+};
