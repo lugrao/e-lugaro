860087edf8cde0d6690c7bd84ead52e024c25dac
lugrao <elugrao@gmail.com>
2023-09-23 17:12:34 -0300

    Add foreground and shadows colors

--- styles/colors.js
+++ styles/colors.js
@@ -4,21 +4,28 @@ It changes with the theme.
 */
 
 export const COLORS = {
-    text: {
-      light: "black",
-      dark: "#dde2f2",
-    },
-    background: {
-      light: "white",
-      dark: "#07162b",
-    },
-    links: {
-      light: "#3d4c84",
-      dark: "#9aa7d7",
-    },
-  }
-  
-  // Useful links:
-  // https://colorhunt.co/palette/0b244719376d576cbca5d7e8
-  // https://maketintsandshades.com/
-  
+  text: {
+    light: "black",
+    dark: "#dde2f2",
+  },
+  background: {
+    light: "white",
+    dark: "#07162b",
+  },
+  foreground: {
+    light: "white",
+    dark: "#122036",
+  },
+  links: {
+    light: "#3d4c84",
+    dark: "#9aa7d7",
+  },
+  shadows: {
+    light: "#e8e8e8",
+    dark: "#061429",
+  },
+};
+
+// Useful links:
+// https://colorhunt.co/palette/0b244719376d576cbca5d7e8
+// https://maketintsandshades.com/
