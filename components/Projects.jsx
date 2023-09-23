0418f6e93c08844d68e6f4b3e5b9fdc68023dcc9
lugrao <elugrao@gmail.com>
2023-09-23 17:04:59 -0300

    Add classes to Image and project container for theming. Move project mage on top of title and description

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -8,38 +8,45 @@ export function Projects(props) {
     <div {...props}>
       <h2>Projects</h2>
       {projectList.map((project, index) => (
-        <div key={index} css={{ padding: "2px 4px 3px", marginBottom: 80 }}>
-          <h3>
-            {project.url ? (
-              <ExternalLink href={project.url} withIcon={true}>
-                {project.title}
-              </ExternalLink>
-            ) : (
-              <>{project.title}</>
-            )}
-          </h3>
-          <p>{project.description}</p>
-          {project.additionalInfoList}
+        <div
+          className="project-box"
+          key={index}
+          css={{
+            marginBottom: 40,
+            borderRadius: 10,
+          }}
+        >
           {project.imageSrc && (
-            <div>
-              <a href={project.url} target="_blank">
-                <Image
-                  src={project.imageSrc}
-                  alt={`${project.title} screen`}
-                  height={344}
-                  width={610}
-                  sizes="100vw"
-                  style={{
-                    objectFit: "contain",
-                    border: "1px solid #b8b8b8",
-                    borderRadius: "5px",
-                    width: "100%",
-                    height: "auto",
-                  }}
-                />
-              </a>
-            </div>
+            <a href={project.url} target="_blank">
+              <Image
+                className="project-image"
+                src={project.imageSrc}
+                alt={`${project.title} screen`}
+                height={344}
+                width={610}
+                sizes="100vw"
+                style={{
+                  objectFit: "contain",
+                  borderRadius: "10px 10px 0 0",
+                  width: "100%",
+                  height: "auto",
+                }}
+              />
+            </a>
           )}
+          <div css={{ padding: "20px 30px 20px" }}>
+            <h3>
+              {project.url ? (
+                <ExternalLink href={project.url} withIcon={true}>
+                  {project.title}
+                </ExternalLink>
+              ) : (
+                <>{project.title}</>
+              )}
+            </h3>
+            <p>{project.description}</p>
+            {project.additionalInfoList}
+          </div>
         </div>
       ))}
     </div>
